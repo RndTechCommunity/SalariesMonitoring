601fc60d3e3fcb4a348c3d787cd947b0dc3a0192
Vadim Martynov <va_martynov@skbkontur.ru>
2023-06-17 23:02:08 +0300

    DbContext stub with ef core

---
+++ src/RndTech.SalariesMonitoring.Database/SalariesMonitoringDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RndTech.SalariesMonitoring.Database;
+
+public class SalariesMonitoringDbContext : DbContext
+{
+    public SalariesMonitoringDbContext(DbContextOptions options) 
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(SalariesMonitoringDbContext).Assembly);
+    }
+
+}
